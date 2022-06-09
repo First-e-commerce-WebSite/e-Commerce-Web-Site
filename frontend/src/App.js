c42e0939bccc03b9a6ecc2b47c4d44caa5a67599
Saif-Ayari <saifayari312@gmail.com>
2022-06-09 14:32:14 +0100

    step 1

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,4 @@
+import data from "./data";
 
 
 function App() {
@@ -7,7 +8,31 @@ function App() {
         <a href="/">a_s_Store.tn</a>
 
       </header>
-      <main>List Products</main>
+      <main>
+        <h1> Featured Products</h1>
+        <div className="products">
+          {
+            data.products.map(product => (
+              <div className="product" key={product.slug}>
+                <a href={`/product/${product.slug}`}>
+                  <img src={product.image} alt={product.name} width="300" height="300" />
+                </a>
+                <div className="product-info" >
+                  <a href={`/product/${product.slug}`}>
+
+                    <p>{product.name}</p>
+                  </a>
+                  <a href={`/product/${product.slug}`}>
+
+                  </a>
+                    <p><strong>{product.price} DT</strong></p>
+                  <button>Add To Cart</button>
+                </div>
+              </div>
+            ))
+          }
+        </div>
+      </main>
     </div>
   );
 }
---
+++ frontend/src/data.js
@@ -0,0 +1,54 @@
+const data = {
+    products: [
+        {
+            name: 'Nike air shoes',
+            slug: 'nike-air',
+            category: 'Shoes',
+            image: '/images/p1.png',
+            price: 290,
+            countInStock: 10,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            description: "high quality shoes"
+        },
+        {
+            name: 'Nike air shoes',
+            slug: 'nike-air-red',
+            category: 'Shoes',
+            image: '/images/p2.png',
+            price: 340,
+            countInStock: 15,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 14,
+            description: "high quality shoes"
+        },
+        {
+            name: 'adidas shoes',
+            slug: 'adidas',
+            category: 'Shoes',
+            image: '/images/p3.png',
+            price: 260,
+            countInStock: 5,
+            brand: 'adidas',
+            rating: 4.5,
+            numReviews: 10,
+            description: "high quality shoes"
+        },
+        {
+            name: 'New Balance shoes',
+            slug: 'new-balance',
+            category: 'Shoes',
+            image: '/images/p4.png',
+            price: 400,
+            countInStock: 15,
+            brand: 'New balance',
+            rating: 4.5,
+            numReviews: 14,
+            description: "high quality shoes"
+        },
+    ]
+}
+
+export default data 
